6f087e30863ca8498f0d8f87e6693cb7d2b37e57
geoffrey jones <bashmntl@gmail.com>
2021-01-25 20:11:35 +0000

    change boiler gpio

--- output_thread.py
+++ output_thread.py
@@ -20,7 +20,7 @@ class BoilerThread(Thread):
     def __init__(self,zones):
         self.zones = zones
         self.interval = 5
-        self.output_gpio = 'P8_11'
+        self.output_gpio = 'P8_8'
         self.config_gpio(self.output_gpio)
         super().__init__()
 
